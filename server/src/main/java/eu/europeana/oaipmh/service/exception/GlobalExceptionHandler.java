9e911747383db85ac575b7d9600089d024d4082b
Marcin Heliński <helin@man.poznan.pl>
2018-04-13 09:37:42 +0200

    EA-1082: changed response status for errors

--- server/src/main/java/eu/europeana/oaipmh/service/exception/GlobalExceptionHandler.java
+++ server/src/main/java/eu/europeana/oaipmh/service/exception/GlobalExceptionHandler.java
@@ -11,8 +11,10 @@ import eu.europeana.oaipmh.service.OaiPmhRequestFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -36,11 +38,44 @@ public class GlobalExceptionHandler extends BaseService {
      * @param e
      * @throws OaiPmhException
      */
-    @ExceptionHandler(OaiPmhException.class)
-    public String handleOaiPmhException(OaiPmhException e, HttpServletRequest request) throws OaiPmhException, JsonProcessingException {
+    @ExceptionHandler({BadArgumentException.class, BadResumptionToken.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleBadRequest(OaiPmhException e, HttpServletRequest request) throws OaiPmhException, JsonProcessingException {
         if (e.doLog()) {
             LOG.error(e.getMessage(), e);
         }
+        return handleException(e, request);
+    }
+
+    /**
+     * Checks if we should log an error and serializes the error response
+     * @param e
+     * @throws OaiPmhException
+     */
+    @ExceptionHandler({BadVerbException.class, CannotDisseminateFormatException.class, IdDoesNotExistException.class, NoRecordsMatchException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFound(OaiPmhException e, HttpServletRequest request) throws OaiPmhException, JsonProcessingException {
+        if (e.doLog()) {
+            LOG.error(e.getMessage(), e);
+        }
+        return handleException(e, request);
+    }
+
+    /**
+     * Checks if we should log an error and serializes the error response
+     * @param e
+     * @throws OaiPmhException
+     */
+    @ExceptionHandler(OaiPmhException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleOther(OaiPmhException e, HttpServletRequest request) throws OaiPmhException, JsonProcessingException {
+        if (e.doLog()) {
+            LOG.error(e.getMessage(), e);
+        }
+        return handleException(e, request);
+    }
+
+    private String handleException(OaiPmhException e, HttpServletRequest request) throws BadArgumentException, JsonProcessingException {
         OAIRequest originalRequest = OaiPmhRequestFactory.createRequest(baseUrl, request.getQueryString(), true);
         OAIError error = new OAIError(e.getErrorCode(), e.getMessage());
         return getXmlMapper().writerWithDefaultPrettyPrinter().
