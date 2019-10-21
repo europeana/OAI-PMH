07680337bd6a86c6642f802ba8dc47fab128be6b
srishti <srishti.singh@europeana.eu>
2019-10-21 15:39:33 +0200

    EA-1704 Implemented the Set Id Validation for allowing only alphanumeric

--- server/src/main/java/eu/europeana/oaipmh/service/exception/GlobalExceptionHandler.java
+++ server/src/main/java/eu/europeana/oaipmh/service/exception/GlobalExceptionHandler.java
@@ -8,17 +8,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
+
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolationException;
-import java.util.List;
 import java.util.stream.Collectors;
 
 /**
