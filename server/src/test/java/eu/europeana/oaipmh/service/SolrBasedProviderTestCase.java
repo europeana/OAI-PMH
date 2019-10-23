85661de2e239998a3d43db1f0c34c28873b8cc84
Patrick Ehlert <patrick.ehlert@europeana.eu>
2019-10-23 09:04:24 +0200

    Make sure we close filestreams

--- server/src/test/java/eu/europeana/oaipmh/service/SolrBasedProviderTestCase.java
+++ server/src/test/java/eu/europeana/oaipmh/service/SolrBasedProviderTestCase.java
@@ -18,20 +18,19 @@ public class SolrBasedProviderTestCase extends BaseApiTestCase {
     @Mock
     protected CloudSolrClient solrClient;
 
-    protected QueryResponse getResponse(String fileName) throws IOException {
-
+    QueryResponse getResponse(String fileName) throws IOException {
         Path path = Paths.get(resDir + "/" + fileName);
-        InputStream body= new FileInputStream(path.toFile());
-        NamedList<Object> result= processResponse(body, null);
-        QueryResponse response = new QueryResponse();
-        response.setResponse(result);
-        return response;
+        try (InputStream body = new FileInputStream(path.toFile())) {
+            NamedList<Object> result = processResponse(body, null);
+            QueryResponse response = new QueryResponse();
+            response.setResponse(result);
+            return response;
+        }
     }
 
     private NamedList<Object> processResponse(InputStream body, Object o) {
         XMLResponseParser parser= new XMLResponseParser();
-        NamedList<Object> result= parser.processResponse(body, "UTF-8");
-        return result;
+        return parser.processResponse(body, "UTF-8");
     }
 
 
