9adf2361c3b269244bf7358bb741baafb07114ff
Patrick Ehlert <patrick.ehlert@europeana.eu>
2024-05-15 16:26:48 +0200

    Fix unit test

--- server/src/test/java/eu/europeana/oaipmh/web/ActuatorTest.java
+++ server/src/test/java/eu/europeana/oaipmh/web/ActuatorTest.java
@@ -1,9 +1,14 @@
 package eu.europeana.oaipmh.web;
 
+import eu.europeana.oaipmh.service.IdentifierProvider;
+import eu.europeana.oaipmh.service.IdentifyProvider;
+import eu.europeana.oaipmh.service.RecordProvider;
+import eu.europeana.oaipmh.service.SetsProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +24,16 @@ public class ActuatorTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    // Mock beans to prevent the need to setup connection to Mongo and Solr
+    @MockBean
+    private RecordProvider recordProvider;
+    @MockBean
+    private IdentifierProvider identifierProvider;
+    @MockBean
+    private IdentifyProvider identifyProvider;
+    @MockBean
+    private SetsProvider setsProvider;
 
     @Test
     public void testActuatorInfo() throws Exception {
